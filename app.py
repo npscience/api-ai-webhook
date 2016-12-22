5f24dfbb33bcd92ee0fd1f53c89d958970fc6a75
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-12-22 11:04:46 +0000

    Add dummy route

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python
 
+import json
 import os
 
 from flask import Flask
+from flask import make_response
+from flask import request
 
 # Flask app should start in global layout
 app = Flask(__name__)
+
+
+@app.route('/')
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print('Request:')
+    print(json.dumps(req, indent=4))
+
+    r = make_response(json.dumps({
+        'speech': 'Say something',
+        'displayText': 'Display something'
+    }, indent=4))
+
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
